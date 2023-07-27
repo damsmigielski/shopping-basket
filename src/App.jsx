f1a55e1411670bac80dc2e2ba71f3ba5fe1bda2c
Damian Smigielski <damsmigielski@gmail.com>
2023-07-27 20:22:12 +0200

    add 1 id product for test with code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const productsData = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  },
+];
 
+function ProductCard({
+  product,
+  handleAddToCart,
+  handleRemoveFromCart,
+  isAdded,
+}) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>price:${product.price}</p>
+      <button
+        onClick={
+          isAdded
+            ? () => handleRemoveFromCart(product)
+            : () => handleAddToCart(product)
+        }>
+        {isAdded ? "Remove" : "Add to card"}
+      </button>
+    </div>
+  );
 }
 
-export default App
+function ProductGrid({
+  products,
+  handleAddToCart,
+  handleRemoveFromCart,
+  addedProducts,
+}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+      }}>
+      {products.map((product) => {
+        const isAdded = addedProducts.some((p) => p.id === product.id);
+        return (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            isAdded={isAdded}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function CartSummary({ products }) {
+  const totalPrice = products.reduce((sum, product) => sum + product.price, 0);
+  return (
+    <div>
+      <h2>Damian Śmigielski: Product Cart</h2>
+      <p>Selected Products: {products.length}</p>
+      <p>Całkowita cena: {totalPrice}</p>
+    </div>
+  );
+}
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((currentCart) => [...currentCart, product]);
+  };
+
+  const removeFromCart = (product) => {
+    setCart((currentCart) => currentCart.filter((p) => p.id !== product.id));
+  };
+
+  return (
+    <div>
+      <CartSummary products={cart} />
+      <ProductGrid
+        products={productsData}
+        handleAddToCart={addToCart}
+        handleRemoveFromCart={removeFromCart}
+        addedProducts={cart}
+      />
+    </div>
+  );
+}
+
+export default App;
