078a0b249575aa4ee5ca7aa0451bab2215041c62
Damian Smigielski <damsmigielski@gmail.com>
2023-07-27 21:15:21 +0200

    add 3 id to test functionality of cards add/remove

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,20 @@ const productsData = [
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
   },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price: 55.99,
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking,camping, mountain/rock climbing, cycling,traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+  },
 ];
 
 function ProductCard({
@@ -28,7 +42,7 @@ function ProductCard({
             ? () => handleRemoveFromCart(product)
             : () => handleAddToCart(product)
         }>
-        {isAdded ? "emove" : "add product"}
+        {isAdded ? "remove" : "add product"}
       </button>
     </div>
   );
